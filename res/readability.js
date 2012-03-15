28567374027335c79db2d95510301fc4cfed5398
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2012-03-16 01:32:57 +0400

    dixsw\d+

--- res/readability.js
+++ res/readability.js
@@ -29,7 +29,7 @@ var readability = {
      * Defined up here so we don't instantiate them repeatedly in loops.
      **/
     regexps: {
-        unlikelyCandidates:    /combx|comment|community|disqus|extra|foot|header|menu|remark|rss|shoutbox|sidebar|sponsor|ad-break|agegate|pagination|pager|popup|social|pageshare|share[_-]?(?:button|link)|sharing|tweet|twitter|(?:facebook|googleplus|linkedin|shorturl)-?button|dsq-content|likeplugin|sharethis|shareit|buzz_|pub_?links|reference|policy_text|hidden|noflash|adsense|cover(?:$|\s)|haberl(?:ist|er)/i,
+        unlikelyCandidates:    /combx|comment|community|disqus|extra|foot|header|menu|remark|rss|shoutbox|sidebar|sponsor|ad-break|agegate|pagination|pager|popup|social|pageshare|share[_-]?(?:button|link)|sharing|tweet|twitter|(?:facebook|googleplus|linkedin|shorturl)-?button|dsq-content|likeplugin|sharethis|shareit|buzz_|pub_?links|reference|policy_text|hidden|noflash|adsense|cover(?:$|\s)|haberl(?:ist|er)|dixsw\d+/i,
         okMaybeItsACandidate:  /and|article|body|column|main|shadow/i,
         positive:              /article|body|content|entry|hentry|main|page|pagination|post|text|blog|story/i,
         negative:              /combx|comment|com-|contact|foot|footer|footnote|masthead|media|meta|outbrain|promo|related|scroll|shoutbox|sidebar|sponsor|shopping|tags|tool|widget|reference|button|navigation/i,
